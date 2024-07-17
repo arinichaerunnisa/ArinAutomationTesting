827ed874dd8f4220089b0d26e6d114a0d0320766
Uswatun251219931 <fajaruswatunchasanah@gmail.com>
2024-07-18 01:02:06 +0700

    lanjutan tugas cypress

---
+++ cypress/e2e/0-personal-arin/login.test.coba.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" /> 
+describe('working with inputs', () => {
+    it('visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+    });
+    it('should fill username', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')
+    })
+    it('should fill password', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.get('input[name=user_password]').clear()
+        cy.get('input[name=user_password]').type('password')
+    })
+    it('should activate checkbox keep me signed in', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.get('input[name=user_remember_me').check()
+        cy.get('input[name=user_remember_me').should('be.checked')
+    })
+})
