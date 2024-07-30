ccdc831a21e69240c19db0019c84994009e39f56
Uswatun251219931 <fajaruswatunchasanah@gmail.com>
2024-07-31 01:14:53 +0700

    revisi fixture

--- cypress/e2e/0-personal-arin/tugas_fixture.cy.js
+++ cypress/e2e/0-personal-arin/tugas_fixture.cy.js
@@ -4,23 +4,19 @@ describe('working with inputs', () => {
         cy.visit('http://zero.webappsecurity.com/login.html')
         cy.url().should('include', 'login.html')
 
-        cy.fixture("latihanuser").then(latihanuser => {
-            const username = latihanuser.username
-            const password = latihanuser.password
+        cy.fixture("user").then(user => {
+            const username = user.username
+            const password = user.password
 
-            cy.get('#user_login').clear()
             cy.get('#user_login').type('username')
-
-            cy.get('input[name=user_password]').clear()
             cy.get('input[name=user_password]').type('password')
-
+            
             cy.get('input[name=user_remember_me').check()
             cy.get('input[name=user_remember_me').should('be.checked')
             
             cy.get('input[name="submit"]').click()
             cy.get('.brand').should('contain.text', 'Zero Bank')
 
-        
     })
         })
           
